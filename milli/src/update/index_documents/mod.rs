c5ad6de44fb7ba50b85ce51b1da61018e64eaf8c
Tamo <tamo@meilisearch.com>
2024-06-20 15:59:32 +0200

    Update arroy and always build the tree that need to be built

--- milli/src/update/index_documents/mod.rs
+++ milli/src/update/index_documents/mod.rs
@@ -547,7 +547,7 @@ where
             pool.install(|| {
                 for k in crate::vector::arroy_db_range_for_embedder(embedder_index) {
                     let writer = arroy::Writer::new(vector_arroy, k, dimension);
-                    if writer.is_empty(wtxn)? {
+                    if !writer.need_build(wtxn)? && writer.is_empty(wtxn)? {
                         break;
                     }
                     writer.build(wtxn, &mut rng, None)?;
